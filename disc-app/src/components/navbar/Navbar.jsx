6234ae02d2f757a902ac3e79eae9fb11da6179b5
Fotis Fotiadis <skioulis@gmail.com>
2025-10-08 17:46:07 +0300

    - Redesign `App.jsx` layout by introducing `header` for better structure. - Update `Navbar.jsx` and `Navbar.css` to align with the new design. - Add global styles in `App.css` for improved consistency. - Include `colors.txt` for reference to new color palette.

--- disc-app/src/components/navbar/Navbar.jsx
+++ disc-app/src/components/navbar/Navbar.jsx
@@ -3,25 +3,27 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <button className="add-button">
-                <span className="plus-sign">+</span>
-            </button>
+        <header>
+            <nav className="navbar">
+                <button className="add-button">
+                    <span className="plus-sign">+</span>
+                </button>
 
-            <div className="right-section">
-                <select className="dropdown">
-                    <option>All</option>
-                    <option>Songs</option>
-                    <option>Artists</option>
-                </select>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="search-input"
-                />
-                <button className="search-button">🔍</button>
-            </div>
-        </nav>
+                <div className="right-section">
+                    <select className="dropdown">
+                        <option>All</option>
+                        <option>Songs</option>
+                        <option>Artists</option>
+                    </select>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="search-input"
+                    />
+                    <button className="search-button">🔍</button>
+                </div>
+            </nav>
+        </header>
     );
 };
 
