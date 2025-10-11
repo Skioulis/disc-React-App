cb7257ed8e457412e7be21ca7397f4411945677d
skioulis <skioulis@gmail.com>
2025-10-11 09:02:16 +0300

    Add ListItem component and basic styling; integrate into App component

--- disc-app/src/App.jsx
+++ disc-app/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import ListItem from "./components/list-item/ListItem.jsx";
 
 function App() {
 
-
   return (
-
     <>
         <Navbar />
+        <ListItem />
     </>
   )
 }
---
+++ disc-app/src/components/list-item/ListItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ListItem.css'
+
+function ListItem() {
+    return (
+        <div className="list-item">
+
+        </div>
+    )
+}
+export default ListItem;
