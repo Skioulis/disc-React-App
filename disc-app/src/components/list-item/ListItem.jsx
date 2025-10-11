50d79273afedf90bd4b734cb16fd930373747e8d
skioulis <skioulis@gmail.com>
2025-10-11 18:37:11 +0300

    Refactor ListItem component styles: update layout with flexbox, reorganize rows, and adjust dimensions for improved structure

--- disc-app/src/components/list-item/ListItem.jsx
+++ disc-app/src/components/list-item/ListItem.jsx
@@ -4,7 +4,13 @@ import './ListItem.css'
 function ListItem() {
     return (
         <div className="list-item">
-            <div className="list-item-first-level">
+            <div className="list-item-first-row">
+                test
+            </div>
+            <div className="list-item-second-row">
+                test
+            </div>
+            <div className="list-item-third-row">
                 test
             </div>
         </div>
