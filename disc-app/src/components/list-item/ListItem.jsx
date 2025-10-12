c65f7de070f959ab95453f164d382cc29844514a
skioulis <skioulis@gmail.com>
2025-10-12 09:43:22 +0300

    Update ListItem styles and structure: implement CSS variables for palette, adjust layout with dynamic sizing and fonts, and standardize class naming

--- disc-app/src/components/list-item/ListItem.jsx
+++ disc-app/src/components/list-item/ListItem.jsx
@@ -2,23 +2,31 @@ import React from 'react';
 import './ListItem.css'
 
 function ListItem() {
+
+    const sampleLyrics = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit voluptate velit esse."
+    const samplePeople =
+        "Sunthetis: Fotiadis \n" +
+        "Stixourgos: Tziouns"
+
     return (
         <div className="list-item">
             <div className="list-item-first-row">
                 <div className="list-item-circle">
-                    <span>d105</span>
+                    <span className="disk-id">d105</span>
                 </div>
                 <div className="list-item-first-row-title">
-                    <span>
+                    <span className="list-item-title">
                         to xasapiko
                     </span>
                 </div>
             </div>
             <div className="list-item-second-row">
-                test
+                <span className="list-item-lyrics">
+                    {sampleLyrics.slice(0, 30) + "..."}
+                </span>
             </div>
             <div className="list-item-third-row">
-                test
+                {samplePeople}
             </div>
         </div>
     )
