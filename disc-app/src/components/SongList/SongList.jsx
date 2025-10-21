240d2941779f2b5a4d83575057606d9d6d942230
Fotis Fotiadis <skioulis@gmail.com>
2025-10-21 12:17:32 +0300

    Add page design diagram, `Disc` and `Person` models, and update styles for better responsiveness and text clamping.

--- disc-app/src/components/SongList/SongList.jsx
+++ disc-app/src/components/SongList/SongList.jsx
@@ -8,12 +8,19 @@ export default function SongList() {
 
     return (
         <>
+            <div className="list-title">
+                <h2>
+                    Τραγούδια
+                </h2>
+            </div>
+            <div className="list-scroll" >
             <ul className="list-container">
             {SampleSongs.map((song) => (
                 <li>
                     <Card key={song.id} song={song}/>
                 </li>))}
             </ul>
+            </div>
         </>
     )
 }
