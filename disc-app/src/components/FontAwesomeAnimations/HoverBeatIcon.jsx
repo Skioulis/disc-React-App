dcf5151cc05da67797ed38798b807938371da973
Fotis Fotiadis <skioulis@gmail.com>
2025-10-23 15:34:28 +0300

    Update `HoverBeatIcon` to dynamically adjust size on hover for improved UX.

--- disc-app/src/components/FontAwesomeAnimations/HoverBeatIcon.jsx
+++ disc-app/src/components/FontAwesomeAnimations/HoverBeatIcon.jsx
@@ -3,15 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function HoverBeatIcon({ icon, className, size = "sm", title }) {
     const [hover, setHover] = useState(false);
+    const [adjSize, setAdjSize] = useState(size);
+
     return (
         <span
             className={className}
             title={title}
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
+            onMouseEnter={() => {
+                setHover(true);
+                setAdjSize("2x");
+            }
+        }
+            onMouseLeave={() => {
+                setHover(false);
+                setAdjSize("sm");
+            }
+        }
             style={{ display: "inline-block" }}
         >
-      <FontAwesomeIcon icon={icon} size={size} beat={hover} />
+      <FontAwesomeIcon icon={icon} size={adjSize} beat={hover} />
     </span>
     );
 }
